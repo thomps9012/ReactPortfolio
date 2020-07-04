5583a6222c13dfb9b43f3414bf29a13c03712b0c
Samuel Thompson <thomps9012@gmail.com>
2020-07-04 05:19:40 -0400

    updated image for calendar

--- reactportfolio/src/portfolioData.js
+++ reactportfolio/src/portfolioData.js
@@ -44,7 +44,7 @@ export default
                 id: 3,//DO NOT CHANGE THIS (Please)😅
                 title: 'Calendar',
                 service: 'Web App',
-                imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                imageSrc: "../public/Images/readmeGenerator.png",
                 url: 'https://thomps9012.github.io/CalendarHW/',
                 repo: 'https://github.com/thomps9012/CalendarHW'
             }
