ef54f59b62e55e81c0cc25973b869e435c230b45
Samuel Thompson <thomps9012@gmail.com>
2020-07-04 04:43:04 -0400

    create app.js structure

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -2,23 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Bio from './components/Bio';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Resume from './components/Resume';
+import Skills from './components/Skills';
+import Work from './components/Work';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <div className="App-header">
+        <Header></Header>
+      </div>
+    <div className="Bio">
+      <Bio></Bio>
+    </div>
+    <div className="Contact">
+      <Contact></Contact>
+    </div>
+    <div className="Skills">
+      <Skills></Skills>
+    </div>
+    <div className="Work">
+      <Work></Work>
+    </div>
+    <div className="Resume">
+      <Resume></Resume>
+    </div>
     </div>
   );
 }
