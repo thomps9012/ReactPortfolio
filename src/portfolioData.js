923f4b23b7d4ed66ce9201d8d9b1e7dcc44d1405
Samuel Thompson <thomps9012@gmail.com>
2020-08-02 09:01:58 -0400

    updated portfolio data

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -2,6 +2,7 @@ import BioPic from "./Images/BioPic.jpg";
 import DogPic from "./Images/dog-1.jpg";
 import NotePic from "./Images/noteMonkey.jpg";
 import CalendarPic from "./Images/readmeGenerator.png";
+import Peoplespurse from "./Images/peopespurse.png";
 
 export default
     {
@@ -21,14 +22,25 @@ export default
         abouttext: "Full Stack Web Developer with experience working in a remote environment alongside individuals from a diverse set of backgrounds with client, server, database, and systems engineering experience. I enjoy learning new practices and applying them to solve novel challenges and problems that arise within the work environment. Thank you for the opportunity to bring a dedicated work ethic and commitment of excellence to your team.",
         aboutImage: BioPic,
         //Skills Text
-        languages: "HTML, AJAX, CSS, JavaScript, SQL, jQuery, Bootstrap, Python, Node.js, HTTP, ReST API, GitHub, Express.js, Sequelize, No SQL, MongoDB, ReactJS",
-        frameworks: "Responsive Design, Object Oriented Programming, Test Driven Development, Express.js, Node Test-Driven Development, State and Props",
+        languages: "HTML, AJAX, CSS, JavaScript, SQL, jQuery, Bootstrap, Python, Node.js, HTTP, ReST API, GitHub, Express.js, Sequelize, No SQL, MongoDB, React, JSX, React Chart.js, Google Fonts",
+        frameworks: "Responsive Design, Object Oriented Programming, Test Driven Development, Node Test-Driven Development, Data Manipulation, Server-Data Connections, State and Prop Components",
         //Change This To Hide The Image of About Section (True Or False)
         ShowAboutImage: true,//true or false (Change Here)
         // Change Projects Here 
         projects: [
             {
                 id: 1,
+                title: "The People's Purse", 
+                description: 'A MERN stack, non-profit application dedicated to collecting data about how individuals would spend their tax dollars. The app features an educational component about the governments different departments in addition to a data visualization piece.', // Add Your Service Type Here
+                //Project Image - Add Your Project Image Here
+                tech: 'Tech Stack: React/JSX, CSS/SCSS, Node, Express, Axios APIs (USAspending), React Chart.js, Sequelize, MySQL DB, Github',
+                imageSrc: Peoplespurse,
+                //Project URL - Add Your Project Url Here
+                url: 'Can be hosted locally',
+                repo: 'https://github.com/evigliucci/final-project'
+            },
+            {
+                id: 2,
                 title: 'Trail Tails', //Project Title - Add Your Project Title Here
                 description: 'A web-based application that helps users find local trails using geolocation or a typed in location. The application will return ten different hiking options based off user preferences once a selection is made the app shows weather and trail conditions.', // Add Your Service Type Here
                 //Project Image - Add Your Project Image Here
@@ -39,7 +51,7 @@ export default
                 repo: 'https://github.com/thomps9012/TrailTails'
             },
             {
-                id: 2,//DO NOT CHANGE THIS (Please)😅
+                id: 3,//DO NOT CHANGE THIS (Please)😅
                 title: 'NoteTaker',
                 description: 'A simple note taking app that allows the user to create and delete notes in an online database.',
                 tech: 'Tech Stack: HTML, CSS, JSON, SQL, Express, and API routes',
@@ -48,7 +60,7 @@ export default
                 repo: 'https://github.com/thomps9012/NoteTaker'
             },
             {
-                id: 3,//DO NOT CHANGE THIS (Please)😅
+                id: 4,//DO NOT CHANGE THIS (Please)😅
                 title: 'Calendar',
                 description: 'A daily calendar application that allows users to schedule out their days from 9-5, changing colors with the hours and saving items in local storage',
                 tech: 'Tech Stack: HTML, CSS, JSON, Local Storage, and API routes',
